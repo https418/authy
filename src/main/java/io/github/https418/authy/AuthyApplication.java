c1bb410ece4a8f0733b87c002e5394b71c51bf88
Sergio Rodriguez <seranrod@bancolombia.com.co>
2025-04-11 10:45:24 -0500

    Authy demo project started

---
+++ src/main/java/io/github/https418/authy/AuthyApplication.java
@@ -0,0 +1,13 @@
+package io.github.https418.authy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/https418/authy/AuthyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.https418.authy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
